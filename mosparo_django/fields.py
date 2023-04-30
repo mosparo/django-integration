8d613029a1fe5faf00d0df957dacfb967cee3fef
Matthias Zobrist <matthias.zobrist@zepi.net>
2023-04-30 11:39:12 +0200

    Added the required and verifiable fields to the verification process

--- mosparo_django/fields.py
+++ mosparo_django/fields.py
@@ -2,7 +2,7 @@ from django import forms
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured, ValidationError
 
-from mosparo import Client
+from mosparo_api_client import Client
 from mosparo_django.config import FrontendConfig
 from mosparo_django.widgets import MosparoWidget
 
@@ -16,8 +16,13 @@ class MosparoField(forms.BooleanField):
     mosparo_private_key = None
     mosparo_verify_ssl = True
 
+    callback_ignored_field_types = None
+    callback_verifiable_field_types = None
+    callback_after_prepare_form_data = None
+
     def __init__(self, mosparo_host=None, mosparo_uuid=None, mosparo_public_key=None, mosparo_private_key=None,
-                 mosparo_verify_ssl=None, *args, **kwargs):
+                 mosparo_verify_ssl=None, callback_ignored_field_types=None, callback_verifiable_field_types=None,
+                 callback_after_prepare_form_data=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         if not isinstance(self.widget, MosparoWidget):
@@ -42,6 +47,10 @@ class MosparoField(forms.BooleanField):
         self.mosparo_private_key = mosparo_private_key
         self.mosparo_verify_ssl = mosparo_verify_ssl
 
+        self.callback_ignored_field_types = callback_ignored_field_types
+        self.callback_verifiable_field_types = callback_verifiable_field_types
+        self.callback_after_prepare_form_data = callback_after_prepare_form_data
+
         frontend_config = FrontendConfig(self.mosparo_host, self.mosparo_uuid, self.mosparo_public_key)
         self.widget.set_frontend_config(frontend_config)
 
@@ -59,14 +68,24 @@ class MosparoField(forms.BooleanField):
         if '_mosparo_submitToken' not in form.data or '_mosparo_validationToken' not in form.data:
             raise ValidationError('mosparo specific fields were not found in the submitted data.')
 
+        data = self.prepare_form_data(form)
         submit_token = form.data['_mosparo_submitToken']
         validation_token = form.data['_mosparo_validationToken']
 
-        res = api_client.verify_submission(form.data.dict(), submit_token, validation_token)
+        res = api_client.verify_submission(data['form_data'], submit_token, validation_token)
 
-        if not res.is_submittable():
-            form.valid = False
-            raise ValidationError('mosparo could not verify the data. Please check the submission in mosparo.')
+        if res != None:
+            verified_fields = res.get_verified_fields()
+            required_field_difference = set(data['required_fields']) - set(verified_fields.keys())
+            verifiable_field_difference = set(data['verifiable_fields']) - set(verified_fields.keys())
+
+            # The submission is only valid if all required and verifiable fields got verified
+            if res.is_submittable() and not required_field_difference and not verifiable_field_difference:
+                form.valid = True
+                return
+
+        form.valid = False
+        raise ValidationError('mosparo could not verify the data. Please check the submission in mosparo.')
 
     def get_mosparo_host(self):
         return self.mosparo_host
@@ -76,3 +95,45 @@ class MosparoField(forms.BooleanField):
 
     def get_mosparo_public_key(self):
         return self.mosparo_public_key
+
+    def prepare_form_data(self, form: forms.Form):
+        data = { 'form_data': {}, 'required_fields': [], 'verifiable_fields': [] }
+        ignored_field_types = [
+            'PasswordInput',
+            'HiddenInput',
+            'MultipleHiddenInput',
+            'FileInput',
+            'ClearableFileInput',
+            'MosparoField',
+        ]
+        if self.callback_ignored_field_types != None:
+            ignored_field_types = self.callback_ignored_field_types(ignored_field_types)
+        
+        verifiable_field_types = [
+            'TextInput',
+            'Textarea',
+            'EmailInput',
+            'URLInput',
+        ]
+        if self.callback_verifiable_field_types != None:
+            verifiable_field_types = self.callback_verifiable_field_types(verifiable_field_types)
+
+        for key, field in form.fields.items():
+            if type(field).__name__ in ignored_field_types or type(field.widget).__name__ in ignored_field_types:
+                continue
+
+            name = key
+
+            if field.required:
+                data['required_fields'].append(name)
+
+            if type(field).__name__ in verifiable_field_types or type(field.widget).__name__ in verifiable_field_types:
+                data['verifiable_fields'].append(name)
+
+            if key in form.data:
+                data['form_data'][key] = form.data[key]
+
+        if self.callback_after_prepare_form_data != None:
+            data = self.callback_after_prepare_form_data(data)
+
+        return data
--- tests/test_MosparoField.py
+++ tests/test_MosparoField.py
@@ -1,9 +1,9 @@
-from django.forms import Form, Widget, CharField
+from django.forms import Form, Widget, CharField, URLField, PasswordInput, TextInput
 from django.core.exceptions import ImproperlyConfigured, ValidationError
 import pytest
 from django.http import QueryDict
 
-from mosparo import RequestHelper, VerificationResult
+from mosparo_api_client import RequestHelper, VerificationResult
 
 from mosparo_django.fields import MosparoField
 
@@ -53,6 +53,7 @@ def test_mosparo_field_verify_data(requests_mock):
     # Prepare the form
     class DummyForm(Form):
         name = CharField(label='Name', max_length=255)
+        password = CharField(label='Password', max_length=255, widget=PasswordInput)
 
     data = QueryDict('name=Test&_mosparo_submitToken=submitToken&_mosparo_validationToken=validationToken')
     form = DummyForm(data=data)
@@ -83,6 +84,144 @@ def test_mosparo_field_verify_data(requests_mock):
     mf = MosparoField()
     mf.verify_data(form)
 
+def test_mosparo_field_verify_data_with_ignored_callback(requests_mock):
+    # Prepare the form
+    class DummyForm(Form):
+        name = CharField(label='Name', max_length=255)
+        password = CharField(label='Password', max_length=255, widget=PasswordInput)
+
+    data = QueryDict('name=Test&_mosparo_submitToken=submitToken&_mosparo_validationToken=validationToken')
+    form = DummyForm(data=data)
+
+    form.is_valid()
+
+    # Prepare the API request
+    public_key = 'public_key'
+    private_key = 'private_key'
+    validation_token = 'validationToken'
+    form_data = {'name': 'Test'}
+
+    request_helper = RequestHelper(public_key, private_key)
+
+    prepared_form_data = request_helper.prepare_form_data(form_data)
+    form_signature = request_helper.create_form_data_hmac_hash(prepared_form_data)
+
+    validation_signature = request_helper.create_hmac_hash(validation_token)
+    verification_signature = request_helper.create_hmac_hash(validation_signature + form_signature)
+
+    requests_mock.post('https://host.local/api/v1/verification/verify', json={
+        'valid': True,
+        'verificationSignature': verification_signature,
+        'verifiedFields': {'name': VerificationResult.FIELD_VALID},
+        'issues': []
+    }, status_code=200)
+
+    def test_callback(types):
+        return []
+
+    mf = MosparoField(callback_ignored_field_types=test_callback)
+
+    with pytest.raises(ValidationError) as exc:
+        # Fails because the password field was not ignored and was missing in the list of verified fields
+        mf.verify_data(form)
+
+    assert 'mosparo could not verify the data. Please check the submission in mosparo.' in str(exc.value)
+
+def test_mosparo_field_verify_data_with_verifiable_callback(requests_mock):
+    class TestInput(TextInput):
+        pass
+
+    # Prepare the form
+    class DummyForm(Form):
+        name = CharField(label='Name', max_length=255)
+        url = URLField(label='URL', max_length=255, required=False)
+        test_field = CharField(label='Test', max_length=255, required=False, widget=TestInput)
+
+    data = QueryDict('name=Test&_mosparo_submitToken=submitToken&_mosparo_validationToken=validationToken')
+    form = DummyForm(data=data)
+
+    form.is_valid()
+
+    # Prepare the API request
+    public_key = 'public_key'
+    private_key = 'private_key'
+    validation_token = 'validationToken'
+    form_data = {'name': 'Test'}
+
+    request_helper = RequestHelper(public_key, private_key)
+
+    prepared_form_data = request_helper.prepare_form_data(form_data)
+    form_signature = request_helper.create_form_data_hmac_hash(prepared_form_data)
+
+    validation_signature = request_helper.create_hmac_hash(validation_token)
+    verification_signature = request_helper.create_hmac_hash(validation_signature + form_signature)
+
+    requests_mock.post('https://host.local/api/v1/verification/verify', json={
+        'valid': True,
+        'verificationSignature': verification_signature,
+        'verifiedFields': {'name': VerificationResult.FIELD_VALID, 'url': VerificationResult.FIELD_VALID},
+        'issues': []
+    }, status_code=200)
+
+    def test_callback(types):
+        # Add TestInput to the list of verifiable fields
+        return types + ['TestInput']
+
+    mf = MosparoField(callback_verifiable_field_types=test_callback)
+
+    with pytest.raises(ValidationError) as exc:
+        mf.verify_data(form)
+
+    # Fails, because not all verifiable fields got verified (the TestInput field type was missing)
+    assert 'mosparo could not verify the data. Please check the submission in mosparo.' in str(exc.value)
+
+def test_mosparo_field_verify_data_with_after_data_callback(requests_mock):
+    class TestInput(TextInput):
+        pass
+
+    # Prepare the form
+    class DummyForm(Form):
+        name = CharField(label='Name', max_length=255)
+        url = URLField(label='URL', max_length=255, required=False)
+        test_field = CharField(label='Test', max_length=255, required=False, widget=TestInput)
+
+    data = QueryDict('name=Test&_mosparo_submitToken=submitToken&_mosparo_validationToken=validationToken')
+    form = DummyForm(data=data)
+
+    form.is_valid()
+
+    # Prepare the API request
+    public_key = 'public_key'
+    private_key = 'private_key'
+    validation_token = 'validationToken'
+    form_data = {'name': 'Test'}
+
+    request_helper = RequestHelper(public_key, private_key)
+
+    prepared_form_data = request_helper.prepare_form_data(form_data)
+    form_signature = request_helper.create_form_data_hmac_hash(prepared_form_data)
+
+    validation_signature = request_helper.create_hmac_hash(validation_token)
+    verification_signature = request_helper.create_hmac_hash(validation_signature + form_signature)
+
+    requests_mock.post('https://host.local/api/v1/verification/verify', json={
+        'valid': True,
+        'verificationSignature': verification_signature,
+        'verifiedFields': {'name': VerificationResult.FIELD_VALID, 'url': VerificationResult.FIELD_VALID},
+        'issues': []
+    }, status_code=200)
+
+    def test_callback(data):
+        data['required_fields'].append('test-field')
+        return data
+
+    mf = MosparoField(callback_after_prepare_form_data=test_callback)
+
+    with pytest.raises(ValidationError) as exc:
+        mf.verify_data(form)
+
+    # Fails, because
+    assert 'mosparo could not verify the data. Please check the submission in mosparo.' in str(exc.value)
 
 def test_mosparo_field_verify_data_validation_error(requests_mock):
     # Prepare the form
